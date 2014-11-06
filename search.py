71b5ad0924edfbfb32b708e4317d3c0f131b7bfe
Sara Packman <sarapackman@gmail.com>
2014-11-06 15:26:53 -0800

    correctly returns list of cancer doctors with search term 'cancer doctor'

--- search.py
+++ search.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, redirect, request, session as websession
 from sqlalchemy import func
 import model
+import re
 
 term_dict = {'bone': 'ortho',
 			 'ear': 'oto',
 			 'throat': 'laryn',
 			 'skin': 'derm',
-			 'heart': 'card'}
+			 'heart': 'card',
+			 'surgery': 'surg',
+			 'surgeon': 'surg', 
+			 'cancer': 'onco',
+			 'blood': 'hema', 
+			 'kidney': 'nephro'}
 
 def search_specialty(term):
 	session = model.connect()	
@@ -18,10 +24,22 @@ def search_specialty(term):
 
 		if word in term_dict.keys():
 			query_words = query_words + '%'+(term_dict[word])+'%'
+		elif re.search('log', word):
+			loc = re.search('log', word)
+			word = word[:loc.start()]
+
+			print "\n\n *************** new word ************** \n\n"
+			print word
+
+			query_words = query_words + '%'+(word)+'%'
+
+		elif word.lower() == 'doctor':
+			continue
 		else:
 			query_words = query_words + '%'+(word)+'%'
 
-	print "**************************** \n\n"+ query_words +"\n\n ************************"
+	print "\n\n ********************* query words ******************** \n\n"
+	print query_words
 
 	dr_list = session.query(model.Provider).filter(model.Provider.specialty.like(query_words)).all()
 
@@ -29,14 +47,14 @@ def search_specialty(term):
 
 	print dr_list
 
-	"\n\n ***************************** List of Specialties ******************* \n\n"
+	print "\n\n ***************************** List of Specialties ******************* \n\n"
 	
 	for dr in dr_list:
 		print dr.specialty
 
 def main():
 	
-	search_specialty("surgery")
+	search_specialty("cancer doctor")
 
 if __name__ == '__main__':
 	main()
