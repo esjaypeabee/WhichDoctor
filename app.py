552e4edef7440312aec8c8a2c0328e1927d0f3f5
Sara Packman <sarapackman@gmail.com>
2014-11-11 15:42:10 -0800

    Code review with Nick Audo. Back end is pretty much done, moving to front end.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, request, session as websession
 from sqlalchemy import func
 import model
+import search
 import numpy
 
 #### IT'S HCPCS
@@ -18,26 +19,29 @@ def search_results():
 	"""Displays a list of doctors matching a speciality in a zipcode (eventually)"""
 	# change this later to be specialty. Get speciality as a dropdown menu??
 	zipcode = request.args.get("zipcode")
-	hcpcs_code = request.args.get("hcpcs-code")
+	search_terms = request.args.get("specialty")
 
 	session = model.connect()
 
 	query = session.query(model.Provider)
 
 	base_avg = None
+	hcpcs_code = None
 
 	# check if user entered anything
-	if zipcode == '' and hcpcs_code == '':
+	if zipcode == '' and search_terms== '':
 		return "Please enter a value"
 
 	else:
 		# if user entered only a zipcode
-		if hcpcs_code != '':
-			base_avg = calc_base_avg(hcpcs_code = hcpcs_code)
-			query = query.join(model.Claim).filter(model.Claim.hcpcs_code == hcpcs_code)
-			hcpcs_code = int(hcpcs_code)
+		if search_terms != '':
+			specialties = search.search_specialty(search_terms)
+			query = query.filter(model.Provider.specialty.in_(specialties))
+			# base_avg = calc_base_avg(hcpcs_code = hcpcs_code)
+			# query = query.join(model.Claim).filter(model.Claim.hcpcs_code == hcpcs_code)
+			# hcpcs_code = int(hcpcs_code)
 		else:
-			hcpcs_code = None
+			specialties = None
 
 		# if user entered only a diagnosis code 
 		if zipcode != '':
@@ -58,24 +62,32 @@ def search_results():
 
 	return render_template('search_results.html', doctor_list = doctor_list)
 
-def calc_base_avg(hcpcs_code=None, doctor_list=None):
+def calc_base_avg(hcpcs_code=None, doctor_list=None, specialties=None):
 	"""With either a treatment code or a list of providers, caluculate the 
 	average submitted charge across all claims of same type or all doctors in 
 	the list."""
 
 	session = model.connect()
 
-	# returns a list of tuples - is there a better way?
+	#finds all doctors in entire city of SF that match that specialty, extracts
+	#list of npi's
+	if specialties:
+		dr_list = session.query(model.Provider).filter(model.Provider.specialty.in_(specialties))
+		npi_list = [doctor.npi for doctor in doctor_list]
+		
+	# KEEPING THIS FOR NOW BUT IS USELESS UNTIL TREATMENT SEARCH WORKS
 	# finds all claims with a given treatment code
 	# if expand beyond SF, change this to optionally limit by geographic area
-	if hcpcs_code:
-		claims = session.query(model.Claim).filter_by(hcpcs_code = hcpcs_code).all()
+	# if hcpcs_code:
+	# 	claims = session.query(model.Claim).filter_by(hcpcs_code = hcpcs_code).all()
 
 	# finds all claims by all doctors in the list
 	if doctor_list:
 		npi_list = [doctor.npi for doctor in doctor_list]
 		# this runs a separate query for each npi in the list - is there a better way?
-		claims = session.query(model.Claim).filter(model.Claim.npi.in_(npi_list)).all()
+		
+
+	claims = session.query(model.Claim).filter(model.Claim.npi.in_(npi_list)).all()
 
 	claim_prices = [claim.avg_submitted_chrg for claim in claims]
 	return sum(claim_prices)/len(claim_prices)
--- search.py
+++ search.py
@@ -3,81 +3,41 @@ from sqlalchemy import func, or_
 import model
 import re
 
-# term_dict = {'bone': ['ortho'],
-# 			 'ear': ['oto'],
-# 			 'throat': ['laryn'],
-# 			 'skin': ['derm'],
-# 			 'heart': ['card'],
-# 			 'surgery': ['surg'],
-# 			 'surgeon': ['surg'],
-# 			 'eye': ['opto', 'opht'],  #oph?t
-# 			 'cancer': ['onco'],
-# 			 'blood': ['hema'], 
-# 			 'kidney': ['nephro'],
-# 			 'stomach': ['gastro']}
-			 # re.compile('chiro'): 'chiro'}
-			 # maybe change these to regex to handle misspellings?
-			 # ophthalmology
-
-toss_words = ['doctor', 'room']
+# words that are common and don't mean anything important
+toss_words = ['doctor', 'room', 'care']
 
 def search_specialty(term):
 	session = model.connect()	
 
+	# split up the search terms
 	term_list = term.split()
-
-	# Load in terms we know about here so we're not querying them multiple times.
-	# db_search_terms = session.query(model.Terms).all()
-	# db_search_terms = [{
-	# 	'term': re.compile(x.regex),
-	# 	'actual': ['%%%s%%' % (y) for y in x.actual_form.split(',')],
-	# 	'procedure': x.procedure}
-	# 	for x in db_search_terms
-	# ]
 
 	query_terms = []
 
 	for word in term_list:
 		word = word.lower()
 
-		# if re.search('ist', word):
-		# 	loc = re.search('ist', word)
-		# 	word = word[:loc.start()]
-
+		#check if word is important or not
 		if word not in toss_words:
-			query_terms.append(word)
+			# add it to list of query terms surrounded by percent signs
+			query_terms.append(word + '%')
 
-		# matched = False
-		# for db_term in db_search_terms:
-		# 	if len(db_term['term'].findall(word)) > 0:
-		# 		query_terms = query_terms + db_term['actual']
-		# 		matched = True
+	#print query_terms
 
-		# if not matched:
-		# 	query_terms.append('%'+(word)+'%')
-
-	print "\n\n ********************* query words ******************** \n\n"
-
-	print query_terms
-	#SELECT * FROM providers WHERE specialty LIKE "%opt%" OR specialty LIKE '%opht%';
-	#dr_list = session.query(Provider).filter(or_ (Provider.specialty.like('%opt%'), Provider.specialty.like('%opht%'))).all()
+	# search for specialties in the lookup table
 	specialty_list = session.query(model.Lookup).filter(*[model.Lookup.search_term.like(x) for x in query_terms]).all()
-	print specialty_list
+	# code for what happens if there is more than one match in the lookup table,
+	# or if there are no matches, goes here.
 	dr_list = session.query(model.Provider).filter(or_(*[model.Provider.specialty.like(lookup.specialty) for lookup in specialty_list])).all()
 
-	print "\n\n ********************* Doctor List ******************** \n\n"
-
-	print dr_list
-
-	print "\n\n ***************************** List of Specialties ******************* \n\n"
-	
-	for dr in dr_list:
-		print dr.specialty
+	# return dr_list
+	# for dr in dr_list:
+	# 	print dr.specialty
+	return [lookup.specialty for lookup in specialty_list]
 
 def main():
 	
-	search_specialty('')
-	# search_specialty("chiroalknvowia;")
+	search_specialty('primary heart')
 
 if __name__ == '__main__':
 	main()
